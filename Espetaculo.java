cb7e17c7b2169ea8684cc50c954f4e1701f792f0
Stephanie Julia Soares Dias <ssoaresdias9802@gmail.com>
2025-11-04 18:12:53 +0000

    assentos

--- Espetaculo.java
+++ Espetaculo.java
@@ -2,5 +2,20 @@
 //retorno de assentos
 //marcar assentos
 public class Espetaculo{
-    
+      String[][] assentos = {
+        {"50", "49", "48", "47", "46", "45", "44", "43", "42", "41"},
+        {"40", "39", "38", "37", "36", "35", "34", "33", "32", "31"},
+        {"30", "29", "28", "27", "26", "25", "24", "23", "22", "21"},
+        {"20", "19", "18", "17", "16", "15", "14", "13", "12", "11"},
+        {"10", "09", "08", "07", "06", "05", "04", "03", "02", "01"}
+    };
+    public void apresentaAssentos(String[][] assentos){
+        System.out.print("|| Assentos Disponíveis ||");
+        for (int i = 0; i < assentos.length; i++) {
+        for (int j = 0; j < assentos[i].length; j++) {
+        System.out.print(assentos[i][j] + " ");
+        }
+        System.out.println();
 }
+    }
+};
